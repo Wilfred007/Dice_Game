4732de6a7ea6986ac4d133389b8f30f61f8459c7
Wilfred007 <adzerwilfred007@hmail.com>
2024-09-17 17:53:23 +0100

    ludo

--- contracts/DIce.sol
+++ contracts/DIce.sol
@@ -30,39 +30,27 @@ contract pseudoRandomLudo {
         playerStatus[msg.sender] = 1;
         numPlayers++;
 
-        // Emit an event to indicate that the player has joined the game
         emit PlayerMoved(msg.sender, 0);
     }
 
-    // Function to roll the dice
     function rollDice() public {
-        // Check if the player is in the game and it's their turn
         require(playerStatus[msg.sender] == 1, "You are not in the game or it's not your turn");
 
-        // Generate a pseudorandom number between 1 and 6
         uint roll = (seed + block.timestamp) % 6 + 1;
 
-        // Update the seed for the pseudorandom generator
         seed = roll;
 
-        // Emit an event to indicate that the player has rolled the dice
         emit DiceRolled(msg.sender, roll);
 
-        // Move the player on the board
         movePlayer(roll);
     }
 
-    // Function to move a player on the board
     function movePlayer(uint roll) internal {
-        // Calculate the new position of the player
         uint newPosition = playerPositions[msg.sender] + roll;
 
-        // Check if the player has finished the game
         if (newPosition >= 52) {
-            // Update the player's status to finished
             playerStatus[msg.sender] = 2;
 
-            // Emit an event to indicate that the player has finished the game
             emit GameFinished(msg.sender);
         } else {
             playerPositions[msg.sender] = newPosition;
