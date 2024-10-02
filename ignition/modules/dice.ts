7ae2c5629c763e3a8c11ed979cbf751e04ac6dc5
Wilfred007 <adzerwilfred007@hmail.com>
2024-10-02 09:08:39 +0100

    deploy script

--- ignition/modules/dice.ts
+++ ignition/modules/dice.ts
@@ -1,9 +1,8 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
-const JAN_1ST_2030 = 1893456000;
-const ONE_GWEI: bigint = 1_000_000_000n;
 
-const pseudoRandomLudo = buildModule("LockModule", (m) => {
+
+const pseudoRandomLudo = buildModule("pseudoRandomLudo", (m) => {
 
 
   const pseudoRandomLudo = m.contract("pseudoRandomLudo");
